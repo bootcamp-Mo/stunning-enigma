42523c2b49882fd6512d0e8369b54865d687cb11
Monique <mo.sherred@gmail.com>
2023-05-20 11:11:46 -0700

    small changes to try and get it to work

--- routes/api/index.js
+++ routes/api/index.js
@@ -9,3 +9,5 @@ router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
 module.exports = router;
+
+
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
-const { sequelize } = require('./models');
+// import sequelize connection
+const db = require("./config/connection");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
+db.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
-});
+})
